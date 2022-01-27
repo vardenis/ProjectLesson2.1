24bff58cbb7163396b7fc0586a4f770febccdb87
Denis <varenikovda@gmail.com>
2022-01-27 13:13:06 +0300

    Initial Commit

---
+++ ProjectLesson2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectLesson2.1
+//
+//  Created by Денис Вареников on 27.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
